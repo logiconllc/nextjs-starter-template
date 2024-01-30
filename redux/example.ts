79b47150919bc16e7e78aa6d600a954db47e006d
Basharat Akram <basharat.akram@logicon.com.pk>
2024-01-30 10:52:18 +0500

    Add Redux folder for reference.

---
+++ redux/example.ts
@@ -0,0 +1,3 @@
+// This diectory will be used for any state management library implementation.
+// Please change the name of the folder according to the library you are using.
+// Try to implement slicers in separate files if needed separatelty for different modules.
